0c4bd3010830a7701f233bf047a6d2824e5e2e91
Ronan Lamour <github@rl.cx>
2017-12-21 01:47:00 +0100

    réponse aux messages mentionnés

--- src/Test.Console/Program.cs
+++ src/Test.Console/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,16 +15,29 @@ namespace Test.Console
 {
     class Program
     {
+        private static DiscordSocketClient _client;
+        
         static async Task Main(string[] args)
         {
-            var discord = new DiscordSocketClient();
-            discord.Log += DiscordOnLog;
+            _client = new DiscordSocketClient();
+            _client.Log += DiscordOnLog;
+            _client.MessageReceived += DiscordOnMessageReceived;
             var token = File.ReadAllText("maitrechoco.token");
-            await discord.LoginAsync(TokenType.Bot, token);
-            await discord.StartAsync();
-
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
 
             await Task.Delay(Timeout.Infinite);
+        }
+
+        private static async Task DiscordOnMessageReceived(SocketMessage msg)
+        {
+            if (msg.Author.Id == _client.CurrentUser.Id) return;
+            
+            if (msg.MentionedUsers.Any(x => x.Id == _client.CurrentUser.Id))
+            {
+                string message = msg.Content.Replace($"{_client.CurrentUser.Id}", msg.Author.Id.ToString());
+                await msg.Channel.SendMessageAsync(message);
+            }
         }
 
         private static Task DiscordOnLog(LogMessage msg)
