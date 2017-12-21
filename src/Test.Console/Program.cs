86a86c81b544b27be57817c1f3e7ccc8954449ef
Ronan Lamour <github@rl.cx>
2017-12-21 01:19:33 +0100

    Test connexion discord

--- src/Test.Console/Program.cs
+++ src/Test.Console/Program.cs
@@ -1,17 +1,35 @@
 ﻿using Discord;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Discord.WebSocket;
+using static System.Console;
 
 namespace Test.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            
+            var discord = new DiscordSocketClient();
+            discord.Log += DiscordOnLog;
+            var token = File.ReadAllText("maitrechoco.token");
+            await discord.LoginAsync(TokenType.Bot, token);
+            await discord.StartAsync();
+
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private static Task DiscordOnLog(LogMessage msg)
+        {
+            WriteLine(msg.Message);
+            return Task.CompletedTask;
         }
     }
 }
