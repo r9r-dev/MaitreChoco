85023cc11deef86e00fe855f9dbae4fb943cd92a
Ronan Lamour <github@rl.cx>
2018-01-09 18:12:03 +0100

    amélioration de la compréhension pour l'invention de mots

--- src/bot.ConsoleClient/Program.cs
+++ src/bot.ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -17,6 +18,7 @@ namespace bot.ConsoleClient
         private static DiscordSocketClient _client;
         private static WeatherClient _wu;
         private static LuisClient _luis;
+        private static WordGenerator _wg = new WordGenerator();
 
         private static readonly string _discordtoken = File.ReadAllText("maitrechoco.token");
         private static readonly string _wutoken = File.ReadAllText("wunderground.token");
@@ -24,65 +26,113 @@ namespace bot.ConsoleClient
 
         static async Task Main(string[] args)
         {
-            //_client = new DiscordSocketClient();
-            //_wu = new WeatherClient(_wutoken);
-            //_luis = new LuisClient("ef487147-41bf-4514-9f70-1c539f24943c", _luistoken, true, "westeurope");
+            _client = new DiscordSocketClient();
+            _wu = new WeatherClient(_wutoken);
+            _luis = new LuisClient("ef487147-41bf-4514-9f70-1c539f24943c", _luistoken, true, "westeurope");
 
-            //_client.Log += DiscordOnLog;
-            //_client.MessageReceived += DiscordOnMessageReceived;
-            
-            //await _client.LoginAsync(TokenType.Bot, _discordtoken);
-            //await _client.StartAsync();
+            _client.Log += DiscordOnLog;
+            _client.MessageReceived += DiscordOnMessageReceived;
 
-            var wg = new WordGenerator();
-            wg.Load(@"D:\dev\github\MaitreChoco\ressources\data\FR.txt");
+            await _client.LoginAsync(TokenType.Bot, _discordtoken);
+            await _client.StartAsync();
 
-            while (true)
+            await Task.Run(async () =>
             {
-                var cnt = int.Parse(ReadKey().KeyChar.ToString());
-                WriteLine();
-                WriteLine(UppercaseFirst(wg.GetWord(cnt)));
-            }
-            
-            
-
+                ReadKey();
+                await _client.StopAsync();
+                _client.Dispose();
+            });
 
             await Task.Delay(Timeout.Infinite);
-        }
-
-        static string UppercaseFirst(string s)
-        {
-            if (string.IsNullOrEmpty(s))
-            {
-                return string.Empty;
-            }
-            char[] a = s.ToCharArray();
-            a[0] = char.ToUpper(a[0]);
-            return new string(a);
         }
 
         private static async Task DiscordOnMessageReceived(SocketMessage msg)
         {
             if (msg.Author.Id == _client.CurrentUser.Id) return;
 
+            if (msg.Content.ToLower().Contains("merci") && msg.MentionedUsers.Any(x => x.Id == _client.CurrentUser.Id))
+            {
+                await msg.Channel.SendMessageAsync($"Ya pas de quoi :yum:");
+                return;
+            }
+
+            var cleanMsg = msg.Content;
+            foreach (var tag in msg.Tags)
+            {
+                cleanMsg = cleanMsg.Replace($"<@{tag.Key}>", "");
+            }
+
             if (msg.Channel.Name == "tests" || msg.Channel.Name == "chocolatines")
             {
-                var prediction = await _luis.Predict(msg.Content);
-                if (prediction.TopScoringIntent.Name == "GetWeather")
+                var prediction = await _luis.Predict(cleanMsg);
+
+                WriteLine($"{msg} ({prediction.TopScoringIntent.Name})");
+
+                switch (prediction.TopScoringIntent.Name)
                 {
-                    if (prediction.Entities.ContainsKey("City"))
-                    {
-                        var city = prediction.Entities["City"].First().Value;
-                        var observation = _wu.GetConditions(QueryType.GlobalCity, new QueryOptions() { Language = "FR", City = city, Country = "France" }).CurrentObservation;
-                        
-                        await msg.Channel.SendMessageAsync($"Il fait {observation.TempC:N0}°C à {observation.DisplayLocation.City}. Le temps est {observation.Weather.ToLower()}.");
-                    }
+                    case "GetWeather":
+                        if (prediction.Entities.ContainsKey("City"))
+                        {
+                            var city = prediction.Entities["City"].First().Value;
+                            var observation = _wu.GetConditions(QueryType.GlobalCity, new QueryOptions() { Language = "FR", City = city, Country = "France" }).CurrentObservation;
+
+                            await msg.Channel.SendMessageAsync($"Il fait {observation.TempC:N0}°C à {observation.DisplayLocation.City}. Le temps est {observation.Weather.ToLower().Replace("pluie", "pluvieux")}.");
+
+                        }
+
+                        break;
+                    case "CreateWord":
+                        var lang = Language.FR;
+                        var langue = "français";
+                        if (prediction.Entities.ContainsKey("WordLanguage"))
+                        {
+                            langue = prediction.Entities["WordLanguage"].First().Value;
+                            switch (prediction.Entities["WordLanguage"].First().Value)
+                            {
+                                case "espagnol":
+                                    lang = Language.ES;
+                                    break;
+                                case "français":
+                                    lang = Language.FR;
+                                    break;
+                                case "italien":
+                                    lang = Language.IT;
+                                    break;
+                                case "suédois":
+                                    lang = Language.SE;
+                                    break;
+                                case "anglais":
+                                    lang = Language.EN;
+                                    break;
+                                default:
+                                    await msg.Channel.SendMessageAsync($"Je ne connais pas la langue \"{langue}\". Je connais l'espagnol, le français, l'italien, le suédois et l'anglais.");
+                                    langue = string.Empty;
+                                    break;
+                            }
+                        }
+
+                        if (!string.IsNullOrEmpty(langue))
+                        {
+                            if (prediction.Entities.ContainsKey("LettersQty"))
+                            {
+                                var qty = int.Parse(prediction.Entities["LettersQty"].First().Value);
+                                if (qty < 3) await msg.Channel.SendMessageAsync($"Je préfère éviter d'inventer des mots de moins de 3 lettres.");
+                                else await msg.Channel.SendMessageAsync($"J'ai inventé le mot {langue} {_wg.GetWord(lang, qty)}.");
+                            }
+                            else
+                            {
+                                await msg.Channel.SendMessageAsync($"J'ai inventé le mot {langue} {_wg.GetWord(lang)}.");
+                            }
+                        }
+                        break;
+                    case "Greetings":
+                        await msg.Channel.SendMessageAsync($"ChocoJour {msg.Author.Mention} !");
+                        break;
+                    case "GoodNight":
+                        await msg.Channel.SendMessageAsync($"Bonne nuit {msg.Author.Mention} !");
+                        break;
                 }
-                else if (prediction.TopScoringIntent.Name == "Greetings")
-                {
-                    await msg.Channel.SendMessageAsync($"ChocoJour {msg.Author.Mention} !");
-                }
-                
+
             }
         }
 
--- src/bot.ConsoleClient/WordGenerator.cs
+++ src/bot.ConsoleClient/WordGenerator.cs
@@ -8,74 +8,120 @@ namespace bot.ConsoleClient
 {
     public class WordGenerator
     {
-        private List<string> _words;
-        private List<string> _firstTwo = new List<string>();
-        private Dictionary<string, List<char>> _letters;
+        private readonly Dictionary<string, LanguageDictionary> _dics;
         private readonly Random _rand;
 
 
         public WordGenerator()
         {
             _rand = new Random();
+            _dics = new Dictionary<string, LanguageDictionary>();
+
+            Load(Language.FR, @"ressources\dictionaries\FR.txt");
+            Load(Language.ES, @"ressources\dictionaries\ES.txt");
+            Load(Language.IT, @"ressources\dictionaries\IT.txt");
+            Load(Language.SE, @"ressources\dictionaries\SE.txt");
+            Load(Language.EN, @"ressources\dictionaries\ENG.txt");
+
         }
 
-
-        public void Load(string file)
+        public void Load(Language lang, string file)
         {
-            _words = File.ReadAllLines(file, Encoding.UTF8)
-                .Select(x => x.Split(" ").First().Split("/").First().Split("\t").First())
-                .Where(x => Char.IsLower(x.First()))
-                .Where(x => x.Length > 2)
-                .Where(x => !x.Contains("-"))
-                .ToList();
-
-            _firstTwo = new List<string>();
-            _letters = new Dictionary<string, List<char>>();
-
-            foreach (var word in _words)
+            var dicName = lang.ToString();
+            try
             {
-                _firstTwo.Add(word.Substring(0, 2));
+                _dics.Add(dicName, new LanguageDictionary());
 
-                for(int i = 2;i<word.Length;i++)
+                _dics[dicName]._words = File.ReadAllLines(file, Encoding.UTF8)
+                    .Select(x => x.Split(" ").First().Split("/").First().Split("\t").First())
+                    .Where(x => Char.IsLower(x.First()))
+                    .Where(x => x.Length > 2)
+                    .Where(x => !x.Contains("-"))
+                    .ToList();
+
+                _dics[dicName]._firstTwo = new List<string>();
+                _dics[dicName]._letters = new Dictionary<string, List<char>>();
+
+                foreach (var word in _dics[dicName]._words)
                 {
-                    var letter = word[i];
-                    var sentence = word.Substring(i - 2, 2);
-                    if (!_letters.ContainsKey(sentence)) _letters.Add(sentence, new List<char>());
-                    _letters[sentence].Add(letter);
-                }
+                    try
+                    {
+                        _dics[dicName]._firstTwo.Add(word.Substring(0, 2));
 
-                var lastSentence = word.Substring(word.Length - 2, 2);
-                if (!_letters.ContainsKey(lastSentence)) _letters.Add(lastSentence, new List<char>());
-                _letters[lastSentence].Add(' ');
+                        for (int i = 2; i < word.Length; i++)
+                        {
+                            var letter = word[i];
+                            var sentence = word.Substring(i - 2, 2);
+                            if (!_dics[dicName]._letters.ContainsKey(sentence)) _dics[dicName]._letters.Add(sentence, new List<char>());
+                            _dics[dicName]._letters[sentence].Add(letter);
+                        }
+
+                        var lastSentence = word.Substring(word.Length - 2, 2);
+                        if (!_dics[dicName]._letters.ContainsKey(lastSentence)) _dics[dicName]._letters.Add(lastSentence, new List<char>());
+                        _dics[dicName]._letters[lastSentence].Add(' ');
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"erreur avec le mot {word} : {e}");
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"erreur avec le dictionnaire {dicName} : {e}");
             }
 
         }
 
-        public string GetWord()
+        public string GetWord(Language lang)
         {
-            var word = _firstTwo[_rand.Next(_firstTwo.Count)];
-
-            var currentSentence = word;
+            var dicName = lang.ToString();
             while (true)
             {
-                var letters = _letters[currentSentence];
-                var nextLetter = letters[_rand.Next(letters.Count)];
-                if (nextLetter != ' ') word += nextLetter;
-                else break;
-                currentSentence = word.Substring(word.Length - 2, 2);
-            }
+                var word = _dics[dicName]._firstTwo[_rand.Next(_dics[dicName]._firstTwo.Count)];
 
-            return word;
+                var currentSentence = word;
+                while (true)
+                {
+                    var letters = _dics[dicName]._letters[currentSentence];
+                    var nextLetter = letters[_rand.Next(letters.Count)];
+                    if (nextLetter != ' ')
+                        word += nextLetter;
+                    else
+                        break;
+                    currentSentence = word.Substring(word.Length - 2, 2);
+                }
+
+                if (_dics[dicName]._words.Contains(word)) continue; // ne peut pas générer un mot existant dans la langue
+                return word;
+            }
         }
 
-        public string GetWord(int length)
+        public string GetWord(Language lang, int length)
         {
             if (length < 3) throw new ArgumentOutOfRangeException(nameof(length));
             while (true)
             {
-                var word = GetWord();
+                var word = GetWord(lang);
                 if (word.Length == length) return word;
             }
         }
+    }
+
+    public class LanguageDictionary
+    {
+        public List<string> _words;
+        public List<string> _firstTwo = new List<string>();
+        public Dictionary<string, List<char>> _letters;
+    }
+
+    public enum Language
+    {
+        FR,
+        ES,
+        IT,
+        SE,
+        EN
     }
 }
